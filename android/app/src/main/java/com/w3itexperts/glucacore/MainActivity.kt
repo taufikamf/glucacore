18ba2332914d043b8f9e2723fa2233e93ecd5966
taufikamf <taufikabdul354@gmail.com>
2025-04-19 17:23:57 +0700

    Add android

--- android/app/src/main/java/com/w3itexperts/glucacore/MainActivity.kt
+++ android/app/src/main/java/com/w3itexperts/glucacore/MainActivity.kt
@@ -2,10 +2,12 @@ package com.w3itexperts.glucacore
 
 import android.os.Build
 import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
@@ -13,7 +15,7 @@ class MainActivity : ReactActivity() {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme)
+    setTheme(R.style.AppTheme);
     super.onCreate(null)
   }
 
@@ -29,29 +31,31 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
-            this,
-            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-            object : DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled) {}
-    )
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
   }
 
   /**
-   * Align the back button behavior with Android S where moving root activities to background
-   * instead of finishing activities.
-   * @see <a
-   * href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
-   */
+    * Align the back button behavior with Android S
+    * where moving root activities to background instead of finishing activities.
+    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+    */
   override fun invokeDefaultOnBackPressed() {
-    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-      if (!moveTaskToBack(false)) {
-        // For non-root activities, use the default implementation to finish them.
-        super.invokeDefaultOnBackPressed()
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+          if (!moveTaskToBack(false)) {
+              // For non-root activities, use the default implementation to finish them.
+              super.invokeDefaultOnBackPressed()
+          }
+          return
       }
-      return
-    }
 
-    // Use the default back button implementation on Android S
-    // because it's doing more than [Activity.moveTaskToBack] in fact.
-    super.invokeDefaultOnBackPressed()
+      // Use the default back button implementation on Android S
+      // because it's doing more than [Activity.moveTaskToBack] in fact.
+      super.invokeDefaultOnBackPressed()
   }
 }
